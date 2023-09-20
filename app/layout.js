687697930ae93342bb242a4b5adc5e44d44f270c
swastiksonkusare <swastik.sonkusare18@gmail.com>
2023-09-20 12:48:22 +0530

    added: responsiveness

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ const RootLayout = ({ children }) => {
         <main className="app">
           <Navbar />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </main>
       </body>
     </html>
